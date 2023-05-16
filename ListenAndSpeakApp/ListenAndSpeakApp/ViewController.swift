c504a016d5ec9227048af888879f022b57432157
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-05-16 11:11:22 +0900

    FIXED: all File delete and add all File

--- ListenAndSpeakApp/ListenAndSpeakApp/ViewController.swift
+++ ListenAndSpeakApp/ListenAndSpeakApp/ViewController.swift
@@ -1,24 +1,17 @@
 //
 //  ViewController.swift
-//  ListenAndSpeakingApp
+//  ListenAndSpeakApp
 //
 //  Created by 이재훈 on 2023/05/16.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
