5b7556ea7997cd37eeaa7f65b29f426be5d3292e
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-05-16 11:07:43 +0900

    git & pod init and add all file

---
+++ ListenAndSpeakingApp/ListenAndSpeakingApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ListenAndSpeakingApp
+//
+//  Created by 이재훈 on 2023/05/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
